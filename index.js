ca16f17a337248904183902360683c3104579d93
Abenezer Chane <abenezerchane@gmail.com>
2024-06-06 09:38:07 -0700

    Users created db

---
+++ index.js
@@ -0,0 +1,16 @@
+express = require('express');
+app = express();
+cors = require('cors');
+require('dotenv').config();
+const { PORT,BASE_URL} = process.env;
+app.use(cors());
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(PORT, () => {
+    console.log(`Example app listening at ${BASE_URL}`);
+});
---
+++ migrations/20240606002003_create_users_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('email').notNullable().unique();
+      table.string('password').notNullable();
+      table.string('name');
+      table.string('username').unique();
+      table.bigInteger('strava_id').unique();
+      table.timestamps(true, true);
+    });
+  };
+  
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('users'); 
+};
