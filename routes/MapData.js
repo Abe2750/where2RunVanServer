55e77f74d069192f2822efd1d984ce59bfa13c97
Abenezer Chane <abenezerchane@gmail.com>
2024-06-09 15:45:23 -0700

    fixed refresh api isssue for strava api

--- routes/MapData.js
+++ routes/MapData.js
@@ -3,19 +3,60 @@ const router = express.Router();
 
 const knex = require("knex")
 const knexConfig = require("../knexfile")
+const axios = require('axios');
 
 
 // route "/mapdata"
+
+// GET strava api key using  client id and client secret
+
+async function getApiKey(clientId, clientSecret, refreshToken) {
+    console.log("jeeeeee")
+    console.log(clientId, clientSecret, refreshToken);
+    try {
+        const response = await axios.post('https://www.strava.com/oauth/token', null, {
+            params: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                grant_type: 'refresh_token',
+                refresh_token: refreshToken
+            }
+        });
+        console.log('Response data:', response.data);
+        return response.data.access_token;
+    } catch (error) {
+        if (error.response) {
+            console.error('Error response data:', error.response.data);
+            console.error('Error response status:', error.response.status);
+            console.error('Error response headers:', error.response.headers);
+        } else if (error.request) {
+            console.error('Error request data:', error.request);
+        } else {
+            console.error('Error message:', error.message);
+        }
+        throw new Error('Failed to get API key');
+    }
+       
+}
+
+const clientId = process.env.STRAVA_CLIENT_ID;
+const clientSecret = process.env.STRAVA_CLIENT_SECRET;
+const refreshToken = process.env.STRAVA_REFRESH_TOKEN;
+
+
+
+
 router.get("/mapdata", async (req, res) => {
     // add location(longtitude and latitude) and apiKey to the response
     try {
-        
+        const StravApiKey = await getApiKey(clientId, clientSecret, refreshToken);
         res.json({
             location: {
                 lat: 49.2827,
                 lng: -123.1207
             },
-            apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+            apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+            stravaApiKey: StravApiKey
         });
 
     }
